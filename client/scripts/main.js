a640e68437d55e120ad4b17cf9355b8b466996c9
Roberto Guerra <uris77@gmail.com>
2014-11-21 23:16:42 -0600

    Converted tabs to spaces

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -10,52 +10,52 @@ require('./members/membersModule');
 
 
 function routes($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('token', {
-			url: '/token',
-			templateUrl: 'views/token.tpl.html',
-			controller: 'TokenCtrl',
-			controllerAs: 'tokenCtrl'
-		})
-		.state('boards', {
-			url: '/boards',
-			templateUrl: 'views/boards.tpl.html',
-			controller: 'TrelloBoardsCtrl',
-			controllerAs: 'boardsCtrl',
-			resolve: {
-				boards: ['TokenHolder', 'TrelloBoards', function(TokenHolder, TrelloBoards){
-					return TrelloBoards.getBoards(TokenHolder.token).then(function(response){
-						return response.data;
-					});
-				}],
-				token: ['TokenHolder', function(TokenHolder){
-					return TokenHolder.token;
-				}]
-			}
-		})
-		.state('members', {
-			url: '/members/:boardId',
-			templateUrl: 'views/members.tpl.html',
-			controller: 'MembersCtrl',
-			controllerAs: 'membersCtrl',
-			resolve: {
-				members: ['$stateParams', 'TokenHolder', 'TrelloBoards', function($stateParams, TokenHolder, TrelloBoards){
-					return TrelloBoards
-								.getMembers($stateParams.boardId, TokenHolder.token)
-								.then(function(response){
-									return response.data;
-								});
-				}]
-			}
-		})
-	;
+    $stateProvider
+        .state('token', {
+            url: '/token',
+            templateUrl: 'views/token.tpl.html',
+            controller: 'TokenCtrl',
+            controllerAs: 'tokenCtrl'
+        })
+        .state('boards', {
+            url: '/boards',
+            templateUrl: 'views/boards.tpl.html',
+            controller: 'TrelloBoardsCtrl',
+            controllerAs: 'boardsCtrl',
+            resolve: {
+                boards: ['TokenHolder', 'TrelloBoards', function(TokenHolder, TrelloBoards){
+                    return TrelloBoards.getBoards(TokenHolder.token).then(function(response){
+                        return response.data;
+                    });
+                }],
+                token: ['TokenHolder', function(TokenHolder){
+                    return TokenHolder.token;
+                }]
+            }
+        })
+        .state('members', {
+            url: '/members/:boardId',
+            templateUrl: 'views/members.tpl.html',
+            controller: 'MembersCtrl',
+            controllerAs: 'membersCtrl',
+            resolve: {
+                members: ['$stateParams', 'TokenHolder', 'TrelloBoards', function($stateParams, TokenHolder, TrelloBoards){
+                    return TrelloBoards
+                                .getMembers($stateParams.boardId, TokenHolder.token)
+                                .then(function(response){
+                                    return response.data;
+                                });
+                }]
+            }
+        })
+    ;
 
 
-	$urlRouterProvider.otherwise('/token');
+    $urlRouterProvider.otherwise('/token');
 }
 
 angular.module('TrelloStandUpRandomizer', 
-	[uiRoute, 'TrelloBoardsService', 'TokenHolderModule','TrelloToken', 'TrelloBoardsModule', 'MembersModule'])
-	.config(routes)
-	.run(function(){
-	});
+    [uiRoute, 'TrelloBoardsService', 'TokenHolderModule','TrelloToken', 'TrelloBoardsModule', 'MembersModule'])
+    .config(routes)
+    .run(function(){
+    });
