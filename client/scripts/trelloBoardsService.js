b850b3cb33a3850bda08441dd51669894a96646f
Roberto Guerra <uris77@gmail.com>
2014-11-21 23:17:20 -0600

    Converted tabs to spaces

--- client/scripts/trelloBoardsService.js
+++ client/scripts/trelloBoardsService.js
@@ -3,15 +3,15 @@
 var angular = require('angular');
 
 function TrelloBoards($http) {
-	return {
-		getMembers: function(boardId, token) {
-			return $http.get('/trello/members/'+boardId+'/'+token);
-		},
-		getBoards: function(token){
-			return $http.get('/trello/boards/' + token);
-		}
-	};
+    return {
+        getMembers: function(boardId, token) {
+            return $http.get('/trello/members/'+boardId+'/'+token);
+        },
+        getBoards: function(token){
+            return $http.get('/trello/boards/' + token);
+        }
+    };
 }
 
 angular.module('TrelloBoardsService', [])
-	.factory('TrelloBoards', TrelloBoards);
+    .factory('TrelloBoards', TrelloBoards);
